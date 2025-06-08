98aeccb4f4ea2db7ccabe60a9649264fc9be5581
Jimmy Kimunyi <jimmy@alluvium.net>
2025-06-08 22:17:30 +0300

    Intergrate changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseService } from './database/database.service';
 import { BooksModule } from './books/books.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
